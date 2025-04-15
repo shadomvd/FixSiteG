e69fad6aaffd8da657101e8c5f69c3d16c7635a8
shadomvd <romandemindov@gmail.com>
2025-04-15 18:03:04 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
+// Импорт Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc, collection, addDoc, getDocs, query, where, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore, doc, setDoc, getDoc, updateDoc, collection, addDoc, getDocs, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Конфигурация Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDmXvrvFGx41c5Ulk9zN1WroeCOfHeoUwE",
     authDomain: "fixsiteg.firebaseapp.com",
@@ -11,32 +13,32 @@ const firebaseConfig = {
     appId: "1:1061245740426:web:d51058e56016c932b49473"
 };
 
-let app, auth, db;
-
 try {
-    app = initializeApp(firebaseConfig);
-    auth = getAuth(app);
-    db = getFirestore(app);
-} catch (error) {
-    console.error('Ошибка инициализации Firebase:', error);
-}
+    // Инициализация приложения Firebase
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const db = getFirestore(app);
 
-export { 
-    auth, 
-    db, 
-    createUserWithEmailAndPassword, 
-    signInWithEmailAndPassword, 
-    signOut, 
-    onAuthStateChanged, 
-    doc, 
-    setDoc, 
-    getDoc, 
-    updateDoc, 
-    collection, 
-    addDoc, 
-    getDocs, 
-    query, 
-    where, // Добавлен экспорт where
-    orderBy, 
-    onSnapshot 
-};
+    // Экспорт всех необходимых функций и объектов
+    export { 
+        auth, 
+        db, 
+        createUserWithEmailAndPassword, 
+        signInWithEmailAndPassword, 
+        signOut, 
+        onAuthStateChanged, 
+        doc, 
+        setDoc, 
+        getDoc, 
+        updateDoc, 
+        collection, 
+        addDoc, 
+        getDocs, 
+        query, 
+        orderBy, 
+        onSnapshot 
+    };
+} catch (error) {
+    console.error("Ошибка инициализации Firebase:", error);
+    throw error;
+}
