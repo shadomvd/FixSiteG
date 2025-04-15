132a933ec0e19c0bd77e4aade97e0eafe38ac4cd
shadomvd <romandemindov@gmail.com>
2025-04-15 18:04:33 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,25 @@
 // Импорт Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc, collection, addDoc, getDocs, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    signOut, 
+    onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { 
+    getFirestore, 
+    doc, 
+    setDoc, 
+    getDoc, 
+    updateDoc, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    orderBy, 
+    onSnapshot 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Конфигурация Firebase
 const firebaseConfig = {
@@ -13,32 +31,34 @@ const firebaseConfig = {
     appId: "1:1061245740426:web:d51058e56016c932b49473"
 };
 
-try {
-    // Инициализация приложения Firebase
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
-    const db = getFirestore(app);
+// Инициализация Firebase
+let app, auth, db;
 
-    // Экспорт всех необходимых функций и объектов
-    export { 
-        auth, 
-        db, 
-        createUserWithEmailAndPassword, 
-        signInWithEmailAndPassword, 
-        signOut, 
-        onAuthStateChanged, 
-        doc, 
-        setDoc, 
-        getDoc, 
-        updateDoc, 
-        collection, 
-        addDoc, 
-        getDocs, 
-        query, 
-        orderBy, 
-        onSnapshot 
-    };
+try {
+    app = initializeApp(firebaseConfig);
+    auth = getAuth(app);
+    db = getFirestore(app);
 } catch (error) {
     console.error("Ошибка инициализации Firebase:", error);
     throw error;
 }
+
+// Экспорт всех необходимых функций и объектов
+export { 
+    auth, 
+    db, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword, 
+    signOut, 
+    onAuthStateChanged, 
+    doc, 
+    setDoc, 
+    getDoc, 
+    updateDoc, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    query, 
+    orderBy, 
+    onSnapshot 
+};
